ab233daf270494d6f2c8711d973da35d46e70a68
Sam <ilakebonza@hotmail.com>
2014-12-05 12:42:10 +0100

    append & appendFile
    uploading files works

--- src/SesameInterface.class.php
+++ src/SesameInterface.class.php
@@ -5,9 +5,9 @@
 * created : 10/11/2014
 * last update : 2/12/2014
 *
-* wrapper
 * Interface with Sesame repository 
-* based on : https://github.com/alexlatchford/phpSesame (using cURL instead)
+* This is a re-writting of phpSesame using cURL instead
+* phpSesame source : https://github.com/alexlatchford/phpSesame 
 *
 * Needs cURL enabled on the server
 */	
@@ -18,10 +18,17 @@
 
 class SesameInterface
 {
+	// Return MIME types
 	const SPARQL_XML = 'application/sparql-results+xml';
-	const SPARQL_POST = 'application/x-www-form-urlencoded'; //if query sent with post
+	//const SPARQL_POST = 'application/x-www-form-urlencoded'; //if query sent with post
+	
+	// Input MIME Types
 	const RDFXML = 'application/rdf+xml';
 	const NTRIPLES = 'text/plain';
+	const TURTLE = 'application/x-turtle';
+	const N3 = 'text/rdf+n3';
+	const TRIX = 'application/trix';
+	const TRIG = 'application/x-trig';
 	
 	private $server;
 	private $repository;
@@ -80,30 +87,87 @@ class SesameInterface
 		return false; //hasn't found the repository
 	}
 	
+	private function checkRepository()
+	{
+		if (empty($this->repository) || $this->repository == '')
+		{
+			throw new Exception ('No repository has been selected.');
+		}
+	}
+	
+	private function checkContext(&$context)
+	{
+		if($context != 'null')
+		{
+			$context = (substr($context, 0, 1) != '<' || substr($context, strlen($context) - 1, 1) != '>') ? "<$context>" : $context;
+			$context = urlencode($context);
+		}
+	}
+	
+	private function checkInputFormat($format)
+	{
+		if ($format != self::RDFXML && $format != self::N3 && $format != self::NTRIPLES
+				&& $format != self::TRIG && $format != self::TRIX && $format != self::TURTLE)
+		{
+			throw new Exception ('Please supply a valid input format.');
+		}
+	}
+	
 	//------------------------------------------------------
 	// 
 	//------------------------------------------------------
 	
 	public function createRepository($name){
-	
-		//creating a repository is only possible from the workbench, weirdly...
-		$serverWorkbench = str_replace("/openrdf-sesame", "/openrdf-workbench", $this->server);  
-		
-		//form data
-		$data = array(
-			'type' => "memory-rdfs" ,
-			'Repository ID' => $name ,
-			'Repository title' => "Repository for the city '" . $name . "' created on " . date("Y-m-d H:i:s"),
-			'Persist' => 'true' ,
-			'Sync delay' => '0' 
-		);
-		
-		$request = new HttpRequest($serverWorkbench . '/repositories/NONE/create', 0, $data);
-		$request->send();
+		if (!$this->existsRepository($name)){
+			//creating a repository is only possible from the workbench, weirdly...
+			$serverWorkbench = str_replace("/openrdf-sesame", "/openrdf-workbench", $this->server);  
+			
+			//form data
+			$data = array(
+				'type' => "memory-rdfs" ,
+				'Repository ID' => $name ,
+				'Repository title' => "Repository for the city '" . $name . "' created on " . date("Y-m-d H:i:s"),
+				'Persist' => 'true' ,
+				'Sync delay' => '0' 
+			);
+			
+			$request = new HttpRequest($serverWorkbench . '/repositories/NONE/create', 0, $data);
+			$request->send();
+		}
 	}
 	
-	//upload xml file
-		//file_get_contents($filePath) 
+	public function append($data, $context = 'null', $inputFormat = self::RDFXML)
+	{
+		$this->checkRepository();
+		$this->checkContext($context);
+		$this->checkInputFormat($inputFormat);
+		
+		$request = new HttpRequest($this->server . '/repositories/' . $this->repository . '/statements?context=' . $context);
+		$request->setHeader('Content-type: ' . $inputFormat);
+		$request->setData($data);
+		$response = $request->send();
+		if($request->getStatus() != 204)
+		{
+			throw new Exception ('Failed to append data to the repository, HTTP response error: ' . $request->getStatus());
+		}
+	}
+	
+	
+	public function appendFile($filePath, $context = 'null', $inputFormat = self::RDFXML)
+	{
+		if(empty($filePath) || $filePath == '')
+		{
+			throw new Exception('Please supply a filepath.');
+		}
+		$data = file_get_contents($filePath);
+
+		if ($context == 'null')
+			$context = "<file://fakepath/$filePath>";
+			
+		$this->append($data, $context, $inputFormat);
+	}
+	
+
 	//upload rdf file
 	//do a query
 	
@@ -158,7 +222,8 @@ class HttpRequest {
 		curl_close($c); //close connection
 		
 		//save output
-		$this->response = $reponse;		
+		$this->response = $reponse;	
+		return $this->response;
 	}
 	
 	/*
@@ -181,13 +246,11 @@ class HttpRequest {
 		$this->header = array($value);
     }
 
-    public function setData ($value ){
-		if (is_array($value)){
-			$this->data = http_build_query($value); //dans array?
-			return true;
-		}
+    public function setData ($value){
+		if (is_array($value))
+			$this->data = http_build_query($value); 
 		else
-			return false;
+			$this->data = $value;
     }
 
 	public function getStatus (){
