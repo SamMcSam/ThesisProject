14385ab78d8e3ea257417590508ce056e32bffc4
Sam <ilakebonza@hotmail.com>
2015-01-23 15:15:47 +0100

    Added **LOADING**
    might remove it, but it's funny

--- src/generateEnrichment.php
+++ src/generateEnrichment.php
@@ -97,7 +97,25 @@ try
 	</head>
 
 	<body>
-		<div id='mainBlock'>
+		<div id='loading'>
+			<div>LOADING...</div> 
+			<div id='loading0'>Analysing specification...</div>
+			<div id='loading1'>Generating request...</div>
+			<div id='loading2' style='display:none;'>Querying triple store...</div>
+			<div id='loading3' style='display:none;'>Analysing results...</div>
+			<div id='loading4' style='display:none;'>Constructing abstract objects...</div>
+			<div id='loading5' style='display:none;'>Using parameters : 
+				<?php 
+					print "<pre>";
+					print_r($parameterList);
+					print "</pre>";
+				?>
+			</div>
+			<div id='loading6' style='display:none;'>Applying layout managers...</div>
+			<div id='loading7' style='display:none;'>Generating concrete objects...</div>
+			<div id='loading8' style='display:none;'>Enriching the scene...</div>
+		</div>
+		<div id='mainBlock' style='display:none;'>
 			<?php 
 				if (empty($errorMessage))
 					echo "<p style='height:400px'><x3d width='600px' height='400px'>" . $output . "</x3d></p>";
@@ -106,11 +124,36 @@ try
 			<p>
 				<?php echo $errorMessage; ?>
 			</p>
-
-			<!--<p>
-				<a href='javascript:history.back()'>Go back</a>
-			</p>-->	
 		</div>
 
 	</body>
 </html>
+
+<script type="text/javascript">
+	var i = 2;
+
+	document.onload = function () {
+		printLog();
+	};
+
+	function printLog(){
+		var max = 800;
+		var min = 300;
+		var wait = Math.floor(Math.random()*(max-min+1)+min);
+
+		if (i < 9){
+			setTimeout(function() {
+				document.getElementById('loading' + i).style.display = '';
+				i++;
+				printLog();
+			}, wait);
+		}
+		else{
+			setTimeout(function() {
+				document.getElementById('loading').style.display = 'none';
+		   	 	document.getElementById('mainBlock').style.display = '';
+			}, 2000);
+		}
+	}
+
+</script>
